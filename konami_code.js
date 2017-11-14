981ca80ac90e5140b27b21deacdde41eb0bcce96
Peter Coyle <coyle.peters@gmail.com>
2017-11-14 02:35:15 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,5 +2,18 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
+  let index = []
 
+  function onKeyDownHandler(e) {
+    const key = (e.detail || e.which)
+    if(index.length<10){
+      index.push(key)
+    } else if(index.length === 10) {
+      index.shift()
+      index.push(key)
+    }
+    if(index === code){
+      alert("Hurray!")
+    }
+  }  
 }
